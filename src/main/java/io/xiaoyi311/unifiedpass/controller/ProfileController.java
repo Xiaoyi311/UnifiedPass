abbb01b6888f80571a7c803aad192777ef24b6fe
Xiaoyi311 <3021002383@qq.com>
2024-01-18 23:41:34 +0800

    Profile Service

--- src/main/java/io/xiaoyi311/unifiedpass/controller/ProfileController.java
+++ src/main/java/io/xiaoyi311/unifiedpass/controller/ProfileController.java
@@ -6,6 +6,7 @@ import io.xiaoyi311.unifiedpass.annotation.Permission;
 import io.xiaoyi311.unifiedpass.entity.User;
 import io.xiaoyi311.unifiedpass.entity.UserError;
 import io.xiaoyi311.unifiedpass.entity.yggdrasil.YggdrasilProfile;
+import io.xiaoyi311.unifiedpass.service.ProfileService;
 import io.xiaoyi311.unifiedpass.service.YggdrasilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -30,7 +32,7 @@ import java.util.UUID;
 @RequestMapping("/api/profile")
 public class ProfileController {
     @Autowired
-    YggdrasilService yggdrasilService;
+    ProfileService profileService;
 
     /**
      * 设置角色信息
@@ -40,69 +42,24 @@ public class ProfileController {
     @PostMapping("setInfo")
     @Permission
     public HttpStatus setInfo(@RequestBody JSONObject data, User user){
-        YggdrasilProfile profile = yggdrasilService.getProfile(user.getProfile());
-        if(profile != null){
-            String model = (String) data.getOrDefault("model", profile.getModel());
-
-            if(!Objects.equals(model, "default") && !Objects.equals(model, "slim")){
-                throw new UserError("lang:profile.unknown_model");
-            }
-
-            if(!Objects.equals(data.getOrDefault("username", profile.getName()), profile.getName())){
-                profile.setName((String) data.getOrDefault("username", profile.getName()));
-                yggdrasilService.setTempInvalid(profile.getUuid());
-            }
-
-            profile.setModel((String) data.getOrDefault("model", profile.getModel()));
-            yggdrasilService.saveProfile(profile);
-        }else{
-            throw new UserError("lang:server.unknown_profile");
-        }
+        profileService.setInfo(
+                user,
+                (String) data.getOrDefault("model", ""),
+                (String) data.getOrDefault("username", "")
+        );
         return HttpStatus.NO_CONTENT;
     }
 
+    /**
+     * 上传皮肤
+     * @param file 文件
+     * @param user 用户
+     * @return 204
+     */
     @PostMapping("uploadSkin")
     @Permission
     public HttpStatus uploadSkin(MultipartFile file, User user){
-        if(file == null || file.isEmpty()){
-            throw new UserError("lang:upload.empty");
-        }
-
-        if(!Objects.equals(file.getContentType(), "image/png")){
-            throw new UserError("lang:upload.unSupport_type");
-        }
-
-        try {
-            BufferedImage bi = ImageIO.read(file.getInputStream());
-            if(bi == null){
-                throw new UserError("lang:upload.noImg");
-            }
-
-            if ((bi.getWidth() % 64 != 0 || bi.getHeight() % 64 != 0) || (bi.getWidth() % 32 != 0 || bi.getHeight() % 32 != 0)) {
-                throw new UserError("lang:upload.unSafe");
-            }
-
-            File folder = new File(System.getProperty("user.dir"), "texture");
-            folder.mkdir();
-
-            YggdrasilProfile profile = yggdrasilService.getProfile(user.getProfile());
-            if(profile.getSkin() != null){
-                File old = new File(folder, profile.getSkin());
-                if(old.exists()){
-                    old.delete();
-                }
-            }
-
-            String filename = OtherUtil.sha256(UUID.randomUUID().toString());
-            File save = new File(folder, filename);
-            file.transferTo(save);
-
-            profile.setSkin(filename);
-            yggdrasilService.saveProfile(profile);
-
-            return HttpStatus.NO_CONTENT;
-        } catch (IOException e) {
-            throw new UserError("lang:upload.fail");
-        }
+        profileService.uploadSkin(file, user);
+        return HttpStatus.NO_CONTENT;
     }
 }
---
+++ src/main/java/io/xiaoyi311/unifiedpass/service/ProfileService.java
@@ -0,0 +1,102 @@
+package io.xiaoyi311.unifiedpass.service;
+
+import io.xiaoyi311.unifiedpass.OtherUtil;
+import io.xiaoyi311.unifiedpass.entity.User;
+import io.xiaoyi311.unifiedpass.entity.UserError;
+import io.xiaoyi311.unifiedpass.entity.yggdrasil.YggdrasilProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * 角色服务
+ * @author xiaoyi311
+ */
+@Service
+public class ProfileService {
+    @Autowired
+    YggdrasilService yggdrasilService;
+
+    /**
+     * 设置用户信息
+     * @param user 用户
+     * @param model_in 模型
+     * @param username_in 用户名
+     */
+    public void setInfo(User user, String model_in, String username_in){
+        YggdrasilProfile profile = yggdrasilService.getProfile(user.getProfile());
+        if(profile != null){
+            String model = Objects.equals(model_in, "") ? profile.getModel() : model_in;
+            String username = Objects.equals(username_in, "") ? profile.getName() : username_in;
+
+            if(!Objects.equals(model, "default") && !Objects.equals(model, "slim")){
+                throw new UserError("lang:profile.unknown_model");
+            }
+
+            if(!Objects.equals(username, profile.getName())){
+                profile.setName(username);
+                yggdrasilService.setTempInvalid(profile.getUuid());
+            }
+
+            profile.setModel(model);
+            yggdrasilService.saveProfile(profile);
+        }else{
+            throw new UserError("lang:server.unknown_profile");
+        }
+    }
+
+    /**
+     * 上传皮肤
+     * @param file 文件
+     * @param user 用户
+     */
+    public void uploadSkin(MultipartFile file, User user){
+        if(file == null || file.isEmpty()){
+            throw new UserError("lang:upload.empty");
+        }
+
+        if(!Objects.equals(file.getContentType(), "image/png")){
+            throw new UserError("lang:upload.unSupport_type");
+        }
+
+        try {
+            BufferedImage bi = ImageIO.read(file.getInputStream());
+            if(bi == null){
+                throw new UserError("lang:upload.noImg");
+            }
+
+            if ((bi.getWidth() % 64 != 0 || bi.getHeight() % 64 != 0) || (bi.getWidth() % 32 != 0 || bi.getHeight() % 32 != 0)) {
+                throw new UserError("lang:upload.unSafe");
+            }
+
+            File folder = new File(System.getProperty("user.dir"), Path.of("texture", "skin").toString());
+            folder.mkdirs();
+
+            YggdrasilProfile profile = yggdrasilService.getProfile(user.getProfile());
+            if(profile.getSkin() != null){
+                File old = new File(folder, profile.getSkin());
+                if(old.exists()){
+                    old.delete();
+                }
+            }
+
+            String filename = OtherUtil.sha256(UUID.randomUUID().toString());
+            File save = new File(folder, filename);
+            file.transferTo(save);
+
+            profile.setSkin(filename);
+            yggdrasilService.saveProfile(profile);
+        } catch (IOException e) {
+            throw new UserError("lang:upload.fail");
+        }
+    }
+}

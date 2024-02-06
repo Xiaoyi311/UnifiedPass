111019930c5da6d5b87f865e2040b49c1957f99c
Xiaoyi311 <3021002383@qq.com>
2024-02-06 19:24:51 +0800

    Code Protected

--- src/main/java/io/xiaoyi311/unifiedpass/UnifiedPassApplication.java
+++ src/main/java/io/xiaoyi311/unifiedpass/UnifiedPassApplication.java
@@ -1,8 +1,12 @@
 package io.xiaoyi311.unifiedpass;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
@@ -19,6 +23,18 @@ public class UnifiedPassApplication {
      * 启动
      */
     public static void main(String[] args) {
-        SpringApplication.run(UnifiedPassApplication.class, args);
+        new SpringApplicationBuilder(UnifiedPassApplication.class)
+                .beanNameGenerator(new CustomBeanNameGenerator())
+                .run(args);
+    }
+
+    /**
+     * 自定义 Bean 生成器
+     */
+    private static class CustomBeanNameGenerator implements BeanNameGenerator {
+        @Override
+        public String generateBeanName(BeanDefinition d, BeanDefinitionRegistry r) {
+            return d.getBeanClassName();
+        }
     }
 }
